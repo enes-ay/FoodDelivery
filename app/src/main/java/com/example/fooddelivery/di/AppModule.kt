cdbe5bc0f8c57ac63197b96124f252f001562256
Enes <enesay233@gmail.com>
2024-09-15 20:09:52 +0300

    Update app module

--- app/src/main/java/com/example/fooddelivery/di/AppModule.kt
+++ app/src/main/java/com/example/fooddelivery/di/AppModule.kt
@@ -1,11 +1,36 @@
 package com.example.fooddelivery.di
 
+import com.example.fooddelivery.data.datasource.FoodsDataSource
+import com.example.fooddelivery.data.repository.FoodsRepository
+import com.example.fooddelivery.retrofit.FoodApiUtils
+import com.example.fooddelivery.retrofit.FoodsDao
+import com.example.fooddelivery.retrofit.RetrofitClient
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 class AppModule {
+
+    @Provides
+    @Singleton
+    fun provideFoodsRepository (foodDS: FoodsDataSource) : FoodsRepository{
+        return FoodsRepository(foodDS)
+    }
+
+    @Provides
+    @Singleton
+    fun provideDataSource (foodsDao: FoodsDao) : FoodsDataSource{
+        return FoodsDataSource(foodsDao)
+    }
+
+    @Provides
+    @Singleton
+    fun provideFoodsDao () : FoodsDao{
+        return FoodApiUtils.getFoodsDao()
+    }
 
 }
